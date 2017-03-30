b68c13de5dbcf95d08abe52e55a2c7404771e9ee
Tom Noogen <tom@tnguyen-imac.local>
2017-03-30 14:54:41 -0500

    not so tiny util

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const nativeForEach = ArrayProto.forEach,
 
 function isNull(obj, defaultValue) {
   return (typeof (obj) === 'undefined' || obj === null) ? defaultValue : obj;
-}
+};
 
 const userAgent = myRoot.navigator.userAgent;
 
@@ -47,7 +47,7 @@ function detectIe() {
 
   // other browser
   return false;
-}
+};
 
 function loadSingleScript(uri, callbackFunc) {
   let tag;
@@ -81,7 +81,7 @@ function loadSingleScript(uri, callbackFunc) {
   }
 
   document.body.appendChild(tag);
-}
+};
 
 export default class Wu {
   constructor() {
@@ -94,6 +94,9 @@ export default class Wu {
     };
 
     this.isNull = isNull;
+    this.win = myRoot;
+    this.doc = document || {};
+
     this.forEach = this.each;
     this.collect = this.map;
     this.any = this.some;
@@ -102,7 +105,99 @@ export default class Wu {
   get name() {
     return this._name;
   }
+
+  /**
+   * cross browser attach event
+   * @param {object} obj     source object
+   * @param {string} evtName event name
+   * @param {object}         self
+   */
+  addEvent(obj, evtName, func) {
+    if (obj.addEventListener) {
+      obj.addEventListener(evtName, func, false);
+    } else if (obj.attachEvent) {
+      obj.attachEvent(evtName, func);
+    } else if (this.getAttribute('on' + evtName)) {
+      obj['on' + evtName] = func;
+    } else {
+      obj[evtName] = func;
+    }
+    return this;
+  }
+
+  /**
+   * cross browser detach event
+   * @param {object} obj     source object
+   * @param {string} evtName event name
+   * @param {object}         self
+   */
+  removeEvent(obj, evtName, func) {
+    if (obj.removeEventListener) {
+      obj.removeEventListener(evtName, func, false);
+    } else if (obj.detachEvent) {
+      obj.detachEvent(evtName, func);
+    } else if (this.getAttribute('on' + evtName)) {
+      obj['on' + evtName] = null;
+    } else {
+      obj[evtName] = null;
+    }
+    return this;
+  }
+
+  /**
+   * cross browser get of xhr
+   * @return {object} the xhr
+   */
+  getAjaxObject() {
+    return ('XMLHttpRequest' in window) ? new XMLHttpRequest() : new window.ActiveXObject('Microsoft.XMLHTTP');
+  }
   
+  /**
+   * helper method to get attribute on specific dom object
+   * @param  {object} dom  element
+   * @param  {string} attr attribute name
+   * @return {string}      attribute value
+   */
+  getAttr(dom, attr) {
+    let el = dom[0] || dom;
+
+    return (el.getAttribute) ? el.getAttribute(attr) : el[attr];
+  }
+
+  getAttrs(dom, attrs) {
+    let rst = {};
+
+    this.each(['', 'data-'], (v, k) => {
+      this.each(attrs || [], (v2, k2) => {
+        let attr = this.getAttribute(dom, v + k2);
+        
+        if (attr) {
+          rst[k2] = attr;
+        }
+      });
+    });
+
+    return rst;
+  }
+
+  /**
+   * helper method to set attribute
+   * @param {[type]} dom   element
+   * @param {[type]} attr  attribute name
+   * @param {[type]} value attribute value
+   */
+  setAttr(dom, attr, value) {
+    let el = dom[0] || dom;
+
+    if (el.setAttribute) {
+      el.setAttribute(attr, value);
+    } else {
+      el[attr] = value;
+    };
+
+    return el;
+  }
+
   contains(obj, target) {
     if (this.isNull(obj, null) === null) return false;
     if (nativeIndexOf && obj.indexOf === nativeIndexOf) return obj.indexOf(target) !== -1;
@@ -110,6 +205,43 @@ export default class Wu {
     return this.any(obj, (value) => {
       return value === target;
     });
+  }
+
+  /**
+   * create an iframe
+   * @return {object} the iframe
+   */
+  createiFrame(id, className) {
+    let iframe = this.doc.createElement('iframe');
+
+    if (id) iframe.id = id;
+    if (className) iframe.className = className;
+    iframe.frameBorder = '0';
+    iframe.marginWidth = '0';
+    iframe.marginHeight = '0';
+    iframe.setAttribute('border', '0');
+    iframe.setAttribute('allowtransparency', 'true');
+    iframe.width = '100%';
+    iframe.height = '100%';
+    return iframe;
+  }
+
+  del(obj, key) {
+    obj[key] = undefined;
+    try {
+      delete obj[key];
+    } catch (e) {
+      let items = {};
+
+      this.each(obj, (v, k) => {
+        if (k !== key) {
+          items[k] = v;
+        }
+      });
+
+      return items;
+    }
+    return obj;
   }
 
   each(obj, iterator, context) {
@@ -292,6 +424,59 @@ export default class Wu {
     return obj;
   }
 
+  /**
+   * helper method to parse querystring
+   * @param  {string} qstr the querystring
+   * @return {object}      result
+   */
+  parseQueryString(qstr) {
+    qstr = (qstr || '').replace('?', '').replace('#', '');
+    let d = decodeURIComponent,
+      query = {},
+      a = qstr.split('&');
+
+    for (let i = 0; i < a.length; i++) {
+      let b = a[i].split('=');
+
+      query[d(b[0])] = d(b[1] || '');
+    }
+    return query;
+  }
+
+  request(opts) {
+    let that = this;
+
+    opts.headers = opts.headers || {};
+    if (['HEAD', 'GET', 'DELETE'].indexOf(opts.method) > -1) {
+      // convert data to query string
+      if (opts.data) {
+        opts.url += (opts.url.indexOf('?') > 0 ? '?' : '&') + that.toQueryString(opts.data);
+        this.del(opts, 'data');
+      }
+    } else if (typeof (opts.data) !== 'string') {
+      // handle non-string content body
+      if ((opts.headers['Content-Type'] + '').indexOf('json') > 0) {
+        opts.data = JSON.stringify(opts);
+      } else {
+        // must be form encoded
+        opts.data = that.toQueryString(opts);
+      }
+    }
+    return that.xhrp(opts);
+  }
+
+  /**
+   * slugify a string
+   * @param  {string} str the string to slug
+   * @return {string}     slug result
+   */
+  slugify(str) {
+    str = str || '';
+    if (str === '') return str;
+    str = str.toLowerCase().replace(/[^0-9a-z\-\_]+/gi, '-').replace(/[\-]+/gi, '-');
+    return str;
+  }
+
   some(obj, predicate, context) {
     let result = false;
 
@@ -326,5 +511,100 @@ export default class Wu {
     }
 
     return collection;
+  }
+
+  /**
+   * reverse object to query string
+   * @param  {object} obj the object
+   * @return {string}     the query string
+   */
+  toQueryString(obj) {
+    let str = '';
+
+    this.each(obj, (v, k) => {
+      str += `&${k}=${encodeURIComponent(v)}`;
+    });
+    return str.replace('&', '');
+  }
+
+  /**
+   * make xhr request with a promise
+   * @param  {object} opts the options
+   * @return {promise}     a promise
+   */
+  xhrp(opts) {
+    let that = this;
+
+    return new Promise((resolve, reject) => {
+      return that.xhr(opts, resolve, reject);
+    });
+  }
+
+  /**
+   * make an xhr request
+   * @param  {object}   options  url string or options object
+   * @param  {Function} callback
+   * @param  {Function} errback  error callback
+   */
+  xhr(options, callback, errback) {
+    let url = options;
+
+    if (typeof url === 'string') {
+      options = options || {};
+      options.url = url;
+    }
+    // Create the XHR request itself
+    let req = this.getAjaxObject();
+
+    if (options.withCredentials) {
+      req.withCredentials = true;
+      if (typeof XDomainRequest !== 'undefined') {
+        // XDomainRequest for IE.
+        req = new XDomainRequest();
+      }
+    }
+    // if there are no options, it failed
+    if (!options || options.length === 0) {
+      errback({
+        xhr: req,
+        error: new Error('xhr expects an url or an options object, none given.')
+      });
+    }
+    // normalize method
+    options.method = options.method || 'GET';
+    // open url
+    req.open(options.method, options.url, req.withCredentials);
+    // set request header
+    this.each(options.headers || {}, (value, key) => {
+      req.setRequestHeader(key, value);
+    });
+    this.addEvent(req, 'readystatechange', () => {
+      if (req.readyState === 4 && (req.status >= 200 && req.status < 400)) {
+        // Callbacks for successful requests
+        callback({
+          xhr: req,
+          text: req.responseText,
+          url: req.responseURL
+        });
+      } else if (req.readyState === 4) {
+        // Callbacks for failed requests
+        errback({
+          xhr: req
+        });
+      }
+      // ignore everything else?
+    });
+    this.addEvent(req, 'error', (err) => {
+      errback({
+        xhr: req,
+        error: err
+      });
+    });
+    // send unless prevent by options
+    // such as user want to handle file upload
+    if (!options.nosend) {
+      req.send(options.data || void 0);
+    }
+    return req;
   }
 }
